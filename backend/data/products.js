b2ea360f45c952291bb203b4924c56a1c859c26c
Bri-man14 <57529346+Bri-man14@users.noreply.github.com>
2021-07-27 12:34:16 -0400

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,53 @@
+const products = [
+    {
+        name:"Rapture",
+        imageUrl:"https://images.squarespace-cdn.com/content/v1/5da73ddedfbed505570d91bb/1604744546109-TDADHGNHVUMZ0C8TCFMC/Rapture%2C+2019.jpg?format=750w",
+        description:"Oil paininting on canvas",
+        price:3000,
+        countInStock :10,   
+    },
+    {
+        name:"Dissolve",
+        imageUrl:"https://images.squarespace-cdn.com/content/v1/5da73ddedfbed505570d91bb/1604744575673-8UDMSC1EN53488YGDTQB/Dissolve%2C2019.jpg?format=500w://images.squarespace-cdn.com/content/v1/5da73ddedfbed505570d91bb/1604744546109-TDADHGNHVUMZ0C8TCFMC/Rapture%2C+2019.jpg?format=750w",
+        description:"Oil paininting on canvas",
+        price:3000,
+        countInStock :10,   
+    },
+    {
+        name:"Question",
+        imageUrl:"https://images.squarespace-cdn.com/content/v1/5da73ddedfbed505570d91bb/1604744632760-0U8XVDNRTI9I364SAMFA/IMG_7191.JPG?format=750w",
+        description:"Oil paininting on canvas",
+        price:3000,
+        countInStock :10,   
+    },
+    {
+        name:"City of Dreams",
+        imageUrl:"https://images.squarespace-cdn.com/content/v1/5da73ddedfbed505570d91bb/1604744663309-PGA4XYV4E7ELJ8CAF7QZ/City+of+Dreams%2C+2019.jpg?format=750w",
+        description:"Oil paininting on canvas",
+        price:3000,
+        countInStock :10,   
+    },
+    {
+        name:"Dream",
+        imageUrl:"https://images.squarespace-cdn.com/content/v1/5da73ddedfbed505570d91bb/1604744721107-805UNEPA6GQ4J1EAEATE/IMG-9189.JPG?format=500w",
+        description:"Oil paininting on canvas",
+        price:3000,
+        countInStock :10,   
+    },
+    {
+        name:"VioletEye",
+        imageUrl:"https://images.squarespace-cdn.com/content/v1/5da73ddedfbed505570d91bb/1604744738397-IA7BTGJJHXCWOXR19JDC/IMG_9364.jpg?format=500w",
+        description:"Oil paininting on canvas",
+        price:3000,
+        countInStock :10,   
+    },
+    {
+        name:"Demon",
+        imageUrl:"https://images.squarespace-cdn.com/content/v1/5da73ddedfbed505570d91bb/1604745094925-0UTA4YNJ0J0IO3Z3PJ7Y/IMG-0616.JPG?format=1000w",
+        description:"Oil paininting on canvas",
+        price:3000,
+        countInStock :10,   
+    },
+];
+
+module.exports = products;
